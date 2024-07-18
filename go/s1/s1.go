6e4191e52608cbf4ce6f8826ae74ebffeedb1588
Dion Vu <dionvu11@gmail.com>
2024-07-18 13:50:15 +0700

    reworded

--- go/s1/s1.go
+++ go/s1/s1.go
@@ -4,11 +4,13 @@ func twoSum(nums []int, target int) []int {
 	m := map[int]int{}
 
 	for i, n := range nums {
-		if v, ok := m[target-n]; ok && i != v {
-			return []int{v, i}
-		}
-
 		m[n] = i
+	}
+
+	for i, n := range nums {
+		if v, ok := m[target-n]; ok && i != v {
+			return []int{i, v}
+		}
 	}
 
 	return nil
